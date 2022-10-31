011d6e9bd10f88cd814ddcd7dc4992ed5c9102eb
sinadavodi <117124411+sinadavodi@users.noreply.github.com>
2022-11-01 01:21:30 +0330

    Add files via upload

---
+++ p41.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main() {
+	
+	float m,n,s;
+	cout<<"enter your number : ";
+	cin>>m;
+	cout<<"enter your sec number : ";
+	cin>>n;
+	s=m+n;
+	cout<<"\n Answer : ";
+	cout<<s;
+	float r,t,j;
+	cout<<"\n enter your number : ";
+	cin>>r;
+	cout<<"enter your sec number : ";
+	cin>>t;
+	j=r*t;
+	cout<<"\n Answer : ";
+	cout<<j;
+	float e,w,q;
+	cout<<"\n enter your number : ";
+	cin>>e;
+	cout<<"enter your sec number : ";
+	cin>>w;
+	q=e-w;
+	cout<<"\n Answer : ";
+	cout<<q;
+	int p,o,i;
+	cout<<"\n enter your number : ";
+	cin>>p;
+	cout<<"enter your sec number : ";
+	cin>>o;
+	i=p%o;
+	cout<<"\n Answer : ";
+	cout<<i;
+}
