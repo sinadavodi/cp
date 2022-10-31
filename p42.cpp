78423d85b2edb244ba275fae346c7d83f7b556c7
sinadavodi <117124411+sinadavodi@users.noreply.github.com>
2022-11-01 02:10:46 +0330

    Add files via upload

---
+++ p42.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main() {
+	int x,y,z,i,n;
+	cout<<"enter your number: ";
+	cin>>x;
+	cout<<"enter your number: ";
+	cin>>y;
+	cout<<"enter your number: ";
+	cin>>z;
+	n=(x-y)*(x+z)/(i=x+y+z);i*i+1;
+	cout<<n;
+}
